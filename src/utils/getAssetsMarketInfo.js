5799facd4282180e31c69b78f3fc5d408fafd0a1
Deepakkurmi-eMoment <deepak.kurmi@emoment.in>
2022-04-23 21:16:32 +0530

    ad

--- src/utils/getAssetsMarketInfo.js
+++ src/utils/getAssetsMarketInfo.js
@@ -1,26 +1,17 @@
-import { createAssetPoolLoader, getConnection } from "@apricot-lend/sdk-ts";
+import { createAssetPoolLoader } from "@apricot-lend/sdk-ts";
 import { TableTokenList } from "../assets/api/Home";
 import { connection } from "../lib/constants/connection";
 
 export const getAssetsMarketInfo = async () => {
   const newAssetsTokenMarket = [];
-  try {
 
-    // let connection = getConnection();
-
+  for (var i = 0; i < TableTokenList.length; i++) {
     let assetPoolLoader = await createAssetPoolLoader(connection);
-    console.log(assetPoolLoader);
-    const fetchPriceObj = await assetPoolLoader.fetchPrice(
-      "BTC"
-    );
-    console.log("fetchPrice", fetchPriceObj);
-    
+
     const AssetsPoolInfoObj = await assetPoolLoader.getAssetPool(
-      "BTC"
+      TableTokenList[i].AssetsName
     );
 
-    console.log("AssetsPoolInfoObj", AssetsPoolInfoObj)
-    
     const {
       depositAptRewardRate,
       depositRate,
@@ -30,82 +21,47 @@ export const getAssetsMarketInfo = async () => {
       borrowRate,
       depositMndeRewardRate,
     } = AssetsPoolInfoObj;
-    
 
-    return [];
+    let DepositAPR = "";
+    let MarketDeposited = "";
+    let MarketBorrowed = "";
 
-    for (var i = 0; i < TableTokenList.length; i++) {
-      let assetPoolLoader = await createAssetPoolLoader(connection);
-      console.log(assetPoolLoader);
-      const fetchPriceObj = await assetPoolLoader.fetchPrice(
-        TableTokenList[i].AssetsName
-      );
-      console.log("fetchPrice", fetchPriceObj);
-      
-      const AssetsPoolInfoObj = await assetPoolLoader.getAssetPool(
-        TableTokenList[i].AssetsName
-      );
-
-      console.log("AssetsPoolInfoObj", AssetsPoolInfoObj)
-      /*
-      const {
-        depositAptRewardRate,
-        depositRate,
-        depositAmount,
-        borrowAmount,
-        borrowAptRewardRate,
-        borrowRate,
-        depositMndeRewardRate,
-      } = AssetsPoolInfoObj;
-
-      let DepositAPR = "";
-      let MarketDeposited = "";
-      let MarketBorrowed = "";
-
-      if (depositMndeRewardRate !== undefined) {
-        DepositAPR =
-          (depositAptRewardRate.d[0] +
-            depositRate.d[0] +
-            depositMndeRewardRate.d[0]) /
-          100000;
-      } else {
-        DepositAPR = (depositAptRewardRate.d[0] + depositRate.d[0]) / 100000;
-      }
-
-      if (TableTokenList[i].AssetsName === "USDC") {
-        MarketDeposited = depositAmount.d[0] + "0." + depositAmount.d[1];
-        let OneValue = borrowAmount.d[0];
-        let secondValue = borrowAmount.d[1];
-        let thirdValueLength = secondValue.toString().length;
-
-        const first1Str = String(secondValue).slice(0, 1);
-        const first1Num = Number(first1Str);
-
-        const lastAllStr = String(secondValue).slice(1, thirdValueLength);
-        const lastAllNum = Number(lastAllStr);
-
-        MarketBorrowed = OneValue + "" + first1Num + "." + lastAllNum;
-      } else {
-        MarketDeposited = depositAmount.d[0];
-        MarketBorrowed = borrowAmount.d[0];
-      }
-
-      const BorrowedAPR = (borrowAptRewardRate.d[0] + borrowRate.d[0]) / 100000;
-
-      const createAssetsMarketPool = {
-        ...TableTokenList[i],
-        DepositAPR,
-        BorrowedAPR,
-        MarketDeposited,
-        MarketBorrowed,
-      };
-
-      newAssetsTokenMarket.push(createAssetsMarketPool); */
+    if (depositMndeRewardRate !== undefined) {
+      DepositAPR =
+        (depositAptRewardRate.d[0] +
+          depositRate.d[0] +
+          depositMndeRewardRate.d[0]) /
+        100000;
+    } else {
+      DepositAPR = (depositAptRewardRate.d[0] + depositRate.d[0]) / 100000;
     }
-      
-  } catch (err) {
-    console.log("Here err", err)
+
+    if (TableTokenList[i].AssetsName === "USDC") {
+      MarketDeposited = depositAmount.d[0] + "0." + depositAmount.d[1];
+      let OneValue = borrowAmount.d[0];
+      let secondValue = borrowAmount.d[1];
+      let thirdValueLength = secondValue.toString().length;
+      const first1Str = String(secondValue).slice(0, 1);
+      const first1Num = Number(first1Str);
+      const lastAllStr = String(secondValue).slice(1, thirdValueLength);
+      const lastAllNum = Number(lastAllStr);
+      MarketBorrowed = OneValue + "" + first1Num + "." + lastAllNum;
+    } else {
+      MarketDeposited = depositAmount.d[0];
+      MarketBorrowed = borrowAmount.d[0];
+    }
+
+    const BorrowedAPR = (borrowAptRewardRate.d[0] + borrowRate.d[0]) / 100000;
+    const createAssetsMarketPool = {
+      ...TableTokenList[i],
+      DepositAPR,
+      BorrowedAPR,
+      MarketDeposited,
+      MarketBorrowed,
+    };
+
+    newAssetsTokenMarket.push(createAssetsMarketPool);
   }
-  
+
   return newAssetsTokenMarket;
 };
